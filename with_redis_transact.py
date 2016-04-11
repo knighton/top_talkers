7bdbb81361b7c47b87ecb589058f5b3acbbac794
James Knighton <iamknighton@gmail.com>
2016-04-11 13:04:44 -0700

    Lock.

--- with_redis_transact.py
+++ with_redis_transact.py
@@ -8,23 +8,37 @@ class RedisTopTalkerTracker(object):
     def is_full_inner(self, redis_table, redis_size):
         return self.client.zcard(redis_table) >= redis_size
 
+    def lock(self, redis_table):
+        pass
+
+    def unlock(self, redis_table):
+        pass
+
     def clear(self, redis_table):
         """
         table -> None
         """
+        self.lock(redis_table)
         self.client.zremrangebyrank(redis_table, 0, -1)
+        self.unlock(redis_table)
 
     def is_full(self, redis_table, redis_size):
         """
         (table, size) -> bool
         """
-        return self.is_full_inner(redis_table, redis_size)
+        self.lock(redis_table)
+        r = self.is_full_inner(redis_table, redis_size)
+        self.unlock(redis_table)
+        return r
 
     def get(self, redis_table, key):
         """
         (table, key) -> count or None
         """
+        self.lock(redis_table)
         count = self.client.zscore(redis_table, key)
+        self.unlock(redis_table)
+
         if count is None:
             return count
 
@@ -34,17 +48,22 @@ class RedisTopTalkerTracker(object):
         """
         (table, key) -> bool
         """
+        self.lock(redis_table)
         count = self.client.zscore(redis_table, key)
+        self.unlock(redis_table)
         return count is not None
 
     def add(self, redis_table, redis_size, key):
         """
         (table, size, key) -> None
         """
+        self.lock(redis_table)
+
         # If it's already in there, increment its count and we're done.
         count = self.client.zscore(redis_table, key)
         if count is not None:
             self.client.zincrby(redis_table, key, 1)
+            self.unlock(redis_table)
             return
 
         # Else if the key is new to us but we're full, pop the lowest key/count
@@ -56,22 +75,28 @@ class RedisTopTalkerTracker(object):
             self.client.zremrangebyrank(redis_table, 0, 0)
             new_count = old_count + 1
             self.client.zadd(redis_table, new_count, key)
+            self.unlock(redis_table)
             return
 
         # Or if the key is new to us and we have space, just insert it.
         self.client.zadd(redis_table, 1, key)
+        self.unlock(redis_table)
 
     def top_n_keys(self, redis_table, n):
         """
         (table, n) -> list of keys
         """
-        return self.client.zrevrange(
+        self.lock(redis_table)
+        rr = self.client.zrevrange(
             redis_table, 0, n - 1, score_cast_func=int)
+        self.unlock(redis_table)
+        return rr
 
     def top_n_keys_counts(self, redis_table, redis_size, n):
         """
         (table, size, n) -> list of (key, count)
         """
+        self.lock(redis_table)
         keys_counts = self.client.zrevrange(
             redis_table, 0, n - 1, withscores=True, score_cast_func=int)
         if self.is_full(redis_table, redis_size):
@@ -80,6 +105,7 @@ class RedisTopTalkerTracker(object):
             the_min = lowest_keys_counts[0][1] - 1
         else:
             the_min = 0
+        self.unlock(redis_table)
         return map(lambda (key, count): (key, count - the_min), keys_counts)
 
 
